15b1c612bd33cccd2fda5a3f6be6f588727e283b
DIPAK-GUPTA <47941136+DIPAK-GUPTA@users.noreply.github.com>
2020-09-28 16:30:59 +0530

    Update sample_run2.py

--- sample_run2.py
+++ sample_run2.py
@@ -5,8 +5,10 @@ Created on Fri Sep 25 21:16:51 2020
 @author: Hindrobo
 """
 
+
+#!pip install flasgger
+
 import pandas as pd 
-import numpy as np
 import pickle
 from flask import Flask, request
 import flasgger
@@ -14,20 +16,18 @@ from flasgger import Swagger
 
 app = Flask(__name__)
 Swagger(app)
-
-pickle_in = open("model_trained.pkl","rb")
-classifier=pickle.load(pickle_in)
+pickle_data = open("model_trained.pkl","rb")
+trained_data = pickle.load(pickle_data)
 
 
 @app.route('/')
 def welcome():
-    return "Welcome All I'm Dipak"
-# this "Welcome All I'm Dipak" will get only when hit the ip 127.0.0.1:5000
-@app.route('/predict',methods=["Get"])
+    return "hello I'm kaggle"
+
+@app.route('/prediction',methods=["Get"])
 def predict_by_single_data_GET_method():
     
-    """Let's Authenticate the Banks Note 
-    This is using docstrings for specifications.
+    """Let's Authenticate the Banks Note .
     ---
     parameters:  
       - name: variance
@@ -55,14 +55,12 @@ def predict_by_single_data_GET_method():
     skewness=request.args.get("skewness")
     curtosis=request.args.get("curtosis")
     entropy=request.args.get("entropy")
-    prediction=classifier.predict([[variance,skewness,curtosis,entropy]])
+    prediction=trained_data.predict([[variance,skewness,curtosis,entropy]])
     print(prediction)
-    return "Hello The answer is"+str(prediction)
-    
-@app.route('/predict_file',methods=["POST"])
+    return "This is your prediction = "+str(prediction)
+@app.route('/prediction_with_file',methods=["POST"])
 def predict_by_file_POST_method():
-    """Let's Authenticate the Banks Note 
-    This is using docstrings for specifications.
+    """Let's Authenticate the Banks Note .
     ---
     parameters:
       - name: file
@@ -72,16 +70,14 @@ def predict_by_file_POST_method():
       
     responses:
         200:
-            description: The output values
+            description: output values
         
     """
     df_test=pd.read_csv(request.files.get("file"))
     print(df_test.head())
-    prediction=classifier.predict(df_test)
+    prediction=trained_data.predict(df_test)
     
     return str(list(prediction))
-
-
 if __name__=='__main__':
     app.run()
 # you need to paste this in your browser
